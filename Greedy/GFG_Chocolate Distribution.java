a60879b066a3d60ec0e76271fc838d175cac0e2e
Gagan Sharma <gagansharma2717@gmail.com>
2021-12-26 16:46:59 +0530

    Greedy Questions

---
+++ Greedy/GFG_Chocolate Distribution.java
@@ -0,0 +1,55 @@
+//Easy
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+            ArrayList<Long> arr = new ArrayList<Long>();
+            for(int i = 0;i<n;i++)
+                {
+                    long x = sc.nextInt();
+                    arr.add(x);
+                }
+            long m = sc.nextLong();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Long> a, long n, long m)
+    {
+        Collections.sort(a);
+        
+        long ans = Long.MAX_VALUE;
+        
+        for(int w = 0 ; w <= (int)(n - m) ; w++)
+        {
+            long max = a.get(w +(int) m -1);
+            long min = a.get(w);
+            
+            ans = Math.min(ans, max - min);
+        }
+        
+        return ans;
+    }
+}
