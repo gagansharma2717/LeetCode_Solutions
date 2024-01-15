3a703ec0093cd11fea5ee234fcaa754e83581c3d
Gagan Sharma <gagansharma2717@gmail.com>
2024-01-15 16:45:59 +0530

    Updates

--- 01_Arrays/14_Remove Duplicates from Sorted Array.java
+++ 01_Arrays/14_Remove Duplicates from Sorted Array.java
@@ -1,17 +1,18 @@
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // Two pointers left and both at index 1 bcoz 0th element cannot be duplicate
-        // left is only for non-duplicate element
-        int left = 1 ; 
-        for(int i = 1 ; i < nums.length ; i++){
-            // if element at ith index is not same as previous element then its new element 
-            // so put it at nums[left] and increment left 
-            if(nums[i] != nums[i-1]){
-              nums[left] = nums[i];
-              left++;
+        if (nums.length == 0) 
+            return 0;
+        
+        int i = 0;
+        
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
-
-        return left;
+        
+        return i + 1;
     }
 }
--- 01_Arrays/22_Leaders in an Array.java
+++ 01_Arrays/22_Leaders in an Array.java
@@ -1,28 +1,17 @@
 class Solution{
    
     static ArrayList<Integer> leaders(int arr[], int n){
-        
-        ArrayList<Integer> res = new ArrayList<>();
-        
-        int temp[] = new int[n];
-        // last element is already a leader mentioned in promblem statement
-        temp[n-1] = arr[n-1];
-        
-        int max = arr[n-1];
-        // finding max for each index element
-        for(int i = n-2; i >= 0 ; i--){
-            max = Math.max(max, arr[i]);
-            temp[i] = max;
-        }
-        
-        // Now the index element if it is greater or equal from its right on that index 
-        // difference will be zero on that index so add it to resultant arraylist 
-        for(int i = 0 ; i <n; i++){
-            if(arr[i] - temp[i] == 0){
-                res.add(arr[i]);
+      
+        int max = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i = n-1; i >= 0 ; i--){
+            if(arr[i] >= max){
+                ans.add(arr[i]);
+                max = arr[i];
             }
         }
         
-        return res;
+        Collections.reverse(ans);
+        return ans;
     }
 }
